f2b55e96747276f139c6ca61109ef5e0f1924f1f
ManupaDev <manupadevsuite@gmail.com>
2025-08-08 13:39:29 +0530

    Add CORS support and new location API. Update package dependencies and scripts for seeding data. Refactor index.js to include CORS middleware and new routes for locations.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,26 @@
 import "dotenv/config";
+
 import express from "express";
+import cors from "cors";
+
 import hotelsRouter from "./api/hotel.js";
 import connectDB from "./infrastructure/db.js";
 import reviewRouter from "./api/review.js";
+import locationsRouter from "./api/location.js";
 
 const app = express();
 
 // Convert HTTP payloads into JS objects
 app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
 
 app.use("/api/hotels", hotelsRouter);
 app.use("/api/reviews", reviewRouter);
+app.use("/api/locations", locationsRouter);
 
 connectDB();
 
