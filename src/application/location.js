b341e59106a0eaf78f42984e6588dc197ceebb3e
ManupaDev <manupadevsuite@gmail.com>
2025-08-17 14:53:05 +0530

    Add authentication middleware and integrate Clerk for user management. Refactor hotel and location APIs to include error handling and validation. Update package dependencies for Clerk integration.

--- src/application/location.js
+++ src/application/location.js
@@ -1,95 +1,94 @@
 import Location from "../infrastructure/entities/Location.js";
+import NotFoundError from "../domain/errors/not-found-error.js";
+import ValidationError from "../domain/errors/validation-error.js";
 
-export const getAllLocations = async (req, res) => {
+export const getAllLocations = async (req, res, next) => {
   try {
     const locations = await Location.find();
     res.status(200).json(locations);
+    return;
   } catch (error) {
-    res.status(500).send();
+    next(error);
   }
 };
 
-export const createLocation = async (req, res) => {
+export const createLocation = async (req, res, next) => {
   try {
+    const userId = req.auth().userId;
+    console.log("USER_ID", userId);
+
     const locationData = req.body;
     if (!locationData.name) {
-      res.status(400).send();
-      return;
+      throw new ValidationError("Location name is required");
     }
     await Location.create(locationData);
     res.status(201).send();
   } catch (error) {
-    res.status(500).send();
+    next(error);
   }
 };
 
-export const getLocationById = async (req, res) => {
+export const getLocationById = async (req, res, next) => {
   try {
     const _id = req.params._id;
     const location = await Location.findById(_id);
     if (!location) {
-      res.status(404).send();
-      return;
+      throw new NotFoundError("Location not found");
     }
     res.status(200).json(location);
   } catch (error) {
-    res.status(500).send();
+    next(error);
   }
 };
 
-export const updateLocation = async (req, res) => {
+export const updateLocation = async (req, res, next) => {
   try {
     const _id = req.params._id;
     const locationData = req.body;
     if (!locationData.name) {
-      res.status(400).send();
-      return;
+      throw new ValidationError("Location name is required");
     }
 
     const location = await Location.findById(_id);
     if (!location) {
-      res.status(404).send();
-      return;
+      throw new NotFoundError("Location not found");
     }
 
     await Location.findByIdAndUpdate(_id, locationData);
     res.status(200).send();
   } catch (error) {
-    res.status(500).send();
+    next(error);
   }
 };
 
-export const patchLocation = async (req, res) => {
+export const patchLocation = async (req, res, next) => {
   try {
     const _id = req.params._id;
     const locationData = req.body;
     if (!locationData.name) {
-      res.status(400).send();
-      return;
+      throw new ValidationError("Location name is required");
     }
     const location = await Location.findById(_id);
     if (!location) {
-      res.status(404).send();
-      return;
+      throw new NotFoundError("Location not found");
     }
     await Location.findByIdAndUpdate(_id, { name: locationData.name });
     res.status(200).send();
   } catch (error) {
-    res.status(500).send();
+    next(error);
   }
 };
 
-export const deleteLocation = async (req, res) => {
+export const deleteLocation = async (req, res, next) => {
   try {
     const _id = req.params._id;
     const location = await Location.findById(_id);
     if (!location) {
-      res.status(404).send();
-      return;
+      throw new NotFoundError("Location not found");
     }
     await Location.findByIdAndDelete(_id);
     res.status(200).send();
   } catch (error) {
-    res.status(500).send();
+    next(error);
   }
-}; 
+};
--- src/infrastructure/entities/Booking.js
+++ src/infrastructure/entities/Booking.js
@@ -2,8 +2,7 @@ import mongoose from "mongoose";
 
 const bookingSchema = new mongoose.Schema({
   userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
+    type: String,
     required: true,
   },
   hotelId: {
